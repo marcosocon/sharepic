df043430f0c5eb848000bdce229a63c982edff4d
marcosocon <oconmarcos97@gmail.com>
2016-02-19 02:40:12 -0300

    added bootstrap and template modifications

--- sharepic.js
+++ sharepic.js
@@ -1,23 +1,28 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  var img_data = [{
+    img_src:"pik.png",
+    img_alt:"pik logo"
+  },
+  {
+    img_src:"pik.png",
+    img_alt:"pik logo"
+  },
+  {
+    img_src:"pik.png",
+    img_alt:"pik logo"
+  },
+  {
+    img_src:"pik.png",
+    img_alt:"pik logo"
+  },
+  {
+    img_src:"pik.png",
+    img_alt:"pik logo"
+  }
+  ];
+  Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  console.log("I'm the Server!");
 }
